2fd2779ad7b7cfaefa27219b76ace0874875a2c9
Adrian Wobito <adrian.wobito@gmail.com>
2024-11-13 10:35:54 -0500

    tokio version
    Signed-off-by: Adrian Wobito <adrian.wobito@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,4 @@ async fn main() {
     let digest = "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
     let re = imageinfo::send_image_info(&image, &digest).await;
     println!("{:?}", re);
-
 }
